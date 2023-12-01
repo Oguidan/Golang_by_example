8eadec42d186107564a45e148eb77b56971568d1
Michael Oguidan <michaeloguidan@gmail.com>
2023-12-01 11:52:45 +0000

    Remove unused code and add variable declarations

--- main.go
+++ main.go
@@ -3,11 +3,27 @@ package main
 import (
 	"fmt"
 	// "math/rand"
-	"math"
+	// "math"
+	// "math/cmplx"
 )
 
 func main() {
 	// fmt.Println("hello, playground")
 	// fmt.Println(rand.Int())
-	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	// fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+
+	// var i int
+	// i = 97
+	// i = 27
+	// var j float32 = 92.30
+	// var k float64 = 48.4994959
+	// var z string = "hello"
+	// var n bool = true
+	// var w complex128 = cmplx.Sqrt(-5 + 12i)
+	// fmt.Println(i, j, k, n, z, w)
+	var i int
+	var f float32
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
 }
