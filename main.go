9aec31597fab6e14bc719fec525d3e13c73329e5
Michael Oguidan <michaeloguidan@gmail.com>
2023-12-01 11:28:28 +0000

    Refactor code to improve performance and readability

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	// "math/rand"
+	"math"
+)
+
+func main() {
+	// fmt.Println("hello, playground")
+	// fmt.Println(rand.Int())
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
