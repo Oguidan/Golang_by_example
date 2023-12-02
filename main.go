b9811477360df33cf97e859c9aac6ec9d9823412
Michael Oguidan <michaeloguidan@gmail.com>
2023-12-02 04:00:12 +0000

    The commit diff introduces the following changes:
    - Variable declarations for `i`, `f`, `b`, and `s` are removed.
    - A variable declaration for `i` with an assignment of `76` is added.
    - A variable declaration for `j` with an assignment of `94` is added.
    - A variable declaration for `k` with an assignment of `43` is added.
    - A commented-out `Println` statement with `i`, `j`, and `k` as arguments is added.
    - Variable declarations for `firstName`, `lastName`, and `employeeId` with assignments of `"ahkil"`, `"sharma"`, and `3` respectively are added.
    - A `Println` statement with `firstName`, `lastName`, and `employeeId` as arguments is added.

--- main.go
+++ main.go
@@ -21,9 +21,26 @@ func main() {
 	// var n bool = true
 	// var w complex128 = cmplx.Sqrt(-5 + 12i)
 	// fmt.Println(i, j, k, n, z, w)
-	var i int
-	var f float32
-	var b bool
-	var s string
-	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+	// var i int
+	// var f float32
+	// var b bool
+	// var s string
+	// fmt.Printf("%v %v %v %q\n", i, f, b, s)
+
+	// var i int
+	// i = 76
+
+	// var j int = 94
+
+	// k := 43
+
+	// fmt.Println(i, j, k)
+
+	var (
+		firstName string = "ahkil"
+		lastname  string = "sharma"
+		employeId int    = 3
+	)
+
+	fmt.Println(firstName, lastname, employeId)
 }
